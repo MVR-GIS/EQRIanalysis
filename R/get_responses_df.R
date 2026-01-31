b425f3da3faa9317d1f831ae46fcbd317b59a145
Michael Dougherty <mike.p.doc.71@gmail.com>
2026-01-31 14:36:58 -0600

    defined ordered responses

--- R/get_responses_df.R
+++ R/get_responses_df.R
@@ -2,10 +2,27 @@
 #' @description Gets a responses data frame.
 #' @returns a data frame of responses
 #' @export
-#' @importFrom here here
+#' @importFrom dplyr mutate
+#' @importFrom forcats as_factor fct_relevel
 get_responses_df <- function() {
   # Get responses
   responses_df <- EQRIanalysis::responses
+
+  # Define factors
+  response_levels <- c(
+    "Yes",
+    "High",
+    "Somewhat High",
+    "Marginal",
+    "Moderate",
+    "Somewhat Low",
+    "Low",
+    "No"
+  )
+  responses_df <- responses_df %>%
+    mutate(RESPONSE = as_factor(RESPONSE)) %>%
+    mutate(RESPONSE = fct_relevel(RESPONSE, response_levels))
+
 
   return(responses_df)
 }
--- tests/testthat/test-get_responses_df.R
+++ tests/testthat/test-get_responses_df.R
@@ -1,5 +1,3 @@
-source(here::here("R/get_responses_df.R"))
-
 testthat::test_that("check that get_responses_df works", {
   responses_df <- get_responses_df()
   testthat::expect_true(is.data.frame(responses_df))
