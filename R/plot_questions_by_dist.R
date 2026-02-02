f3a1ff1088fc667ef6d4c7dfbcf64f209e265b75
Michael Dougherty <mike.p.doc.71@gmail.com>
2026-02-02 14:36:02 -0600

    facet plot_questions_by_dist using patchwork

---
+++ R/plot_questions_by_dist.R
@@ -0,0 +1,68 @@
+#' @title Plot Question by USACE Districts
+#' @description Plot question by USACE Districts.
+#' @param questions_df     data.frame; A data frame of questions
+#'                         returned by `get_questions_df`.
+#' @param responses_df     data.frame; A data frame of responses
+#'                         returned by `get_responses_df`.
+#' @param question_number  integer; The question number to be ploted.
+#' @returns a ggplot2 object
+#' @export
+#' @importFrom dplyr %>% filter mutate
+#' @importFrom forcats fct_drop
+#' @importFrom purrr map
+#' @importFrom ggplot2 ggplot aes geom_bar scale_fill_manual vars
+#'                     theme_grey theme element_blank
+#'                     facet_wrap
+#' @importFrom NatParksPalettes natparks.pals
+#' @importFrom patchwork wrap_plots
+#'
+plot_question_by_dist <- function(questions_df, responses_df, question_number) {
+  # Extract the current question
+  current_question <- questions_df %>%
+    filter(QUESTION_NUMBER == question_number)
+
+  # Filter responses for a single question
+  question_responses <- responses_df %>%
+    filter(QUESTION_NUMBER == current_question$QUESTION_NUMBER) %>%
+    # Drop unused factor levels
+    mutate(RESPONSE = fct_drop(RESPONSE))
+
+  # Determine question number of responses
+  response_levels <- nlevels(question_responses$RESPONSE)
+
+  # Determine the Divisions in the data
+  divisions <- levels(fct_drop(question_responses$DIVISION))
+
+  # Create list of plots
+  plot_list <- map(divisions, function(division) {
+    # Filter for the division
+    question_responses_div <- question_responses %>%
+      filter(DIVISION == division)
+
+    ggplot(question_responses_div, 
+      aes(x = RESPONSE, fill = RESPONSE)) +
+    geom_bar(show.legend = FALSE) +
+    scale_fill_manual(
+      values = natparks.pals("Arches", n = response_levels, 
+                             type = "continuous")
+    ) +
+    facet_grid(cols = vars(DISTRICT)) +   # Facet by District
+    theme_grey(base_size = 11) +
+    theme(
+      axis.text.x = element_blank(),
+      axis.ticks.x = element_blank(),
+      axis.title.x = element_blank(),
+      legend.position = "bottom",
+      legend.title = element_blank()
+    )
+  })
+    
+  plot <- wrap_plots(
+      plot_list, 
+      nrow = length(divisions),
+      guides = "collect"
+    ) & 
+    theme()
+  plot
+  return(plot)
+}
